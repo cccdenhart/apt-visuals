8aef2ab2ac3bac1d91df51d9f3ed4e932421f61c
cccdenhart <cccdenhart@me.com>
2018-11-19 01:47:58 -0500

    added air data

--- main.js
+++ main.js
@@ -20,13 +20,55 @@ map.on("load", function() {
   });
 
   map.addLayer({
-    id: "apts_dens",
+    id: "apt_dens",
     type: "fill",
     source: "apts",
     layout: {},
     paint: {
+      "fill-color": "#008000",
+      "fill-opacity": ["get", "density"]
+    }
+  });
+
+  map.addLayer({
+    id: "air_dens",
+    type: "fill",
+    source: "airs",
+    layout: {},
+    paint: {
       "fill-color": "#FF0000",
-      "fill-opacity": 0.5471698113207548
+      "fill-opacity": ["get", "density"]
     }
   });
 });
+
+var toggleableLayerIds = ["apt_dens", "air_dens"];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+  var id = toggleableLayerIds[i];
+
+  var link = document.createElement("button");
+  link.href = "#";
+  link.className = "active";
+  link.textContent = id;
+  link.innerHTML = id;
+
+  link.onclick = function(e) {
+    var clickedLayer = this.textContent;
+    e.preventDefault();
+    e.stopPropagation();
+
+    var visibility = map.getLayoutProperty(clickedLayer, "visibility");
+
+    if (visibility === "visible") {
+      map.setLayoutProperty(clickedLayer, "visibility", "none");
+      this.className = "";
+    } else {
+      this.className = "active";
+      map.setLayoutProperty(clickedLayer, "visibility", "visible");
+    }
+  };
+
+  var layers = document.getElementById("menu");
+  layers.appendChild(link);
+}
